1d44a655395abc06759c955d074c1dd73f113f76
Marek Faltysek <marek.faltysek@praguematica.co.uk>
2018-06-27 14:27:19 +0100

    adding md scaffold functionality

---
+++ test-scaffold.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+node lib/md.js scaffold -o tpl-vue-portlet-out
--- test-server.sh
+++ test-server.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-node lib/md.js scaffold -o tpl-vue-portlet-out -t vue-portlet
 node lib/md.js server init -f
 node lib/md.js server start
